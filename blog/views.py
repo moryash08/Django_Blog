04c6f63245857268bba8c0a6685a8236908b6a47
Yash Mor <yash.mor@spit.ac.in>
2020-10-18 01:13:17 +0530

    User Registration Form

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'Blake John',
-        'title': 'Blog Post Test 1',
-        'content': 'This is a test post.',
-        'date_posted': 'October 11, 2020'
-    },
-    {
-        'author': 'Sarah Williams',
-        'title': 'Blog Post Test 2',
-        'content': 'This is test post 2.',
-        'date_posted': 'October 9, 2020'
-    }
-]
-
-
 # Create your views here.
 def home(request):
     context = {
